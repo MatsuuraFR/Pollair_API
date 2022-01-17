fb32ece9f2447834bbecdf82de8fee2bcf707b9e
MatsuuraFR <52784006+MatsuuraFR@users.noreply.github.com>
2022-01-17 11:49:10 +0100

    commit matin
    route broken, a revoir

--- src/main/java/application/PollairAPIApplication.java
+++ src/main/java/application/PollairAPIApplication.java
@@ -1,13 +1,35 @@
 package application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages="controller")
-public class PollairAPIApplication {
+import service.FilesStorageService;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.CommandLineRunner;
+import javax.annotation.Resource;
 
+@SpringBootApplication(scanBasePackages="controller")
+@ComponentScan("service")
+public class PollairAPIApplication implements CommandLineRunner {
+	
+	@Resource
+	FilesStorageService storageService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PollairAPIApplication.class, args);
 	}
 	
+	
+	@Override
+	public void run(String... arg) throws Exception {
+	  //storageService.deleteAll();
+	  //storageService.init();
+		try {
+			storageService.init();
+		}catch(Exception e) {
+			System.err.println(e);
+		}
+	}
 }
---
+++ src/main/java/controller/UploadFileHandler.java
@@ -0,0 +1,72 @@
+package controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import Tools.FileInfo;
+import service.FilesStorageService;
+
+//import com.bezkoder.spring.files.upload.model.FileInfo;
+//import com.bezkoder.spring.files.upload.model.ResponseMessage;
+//import com.bezkoder.spring.files.upload.service.FilesStorageService;
+
+@RestController
+@RequestMapping(path = "/fileshandler")
+//@CrossOrigin("http://localhost:8080")
+public class UploadFileHandler {
+	
+	@Autowired
+	FilesStorageService storageService;
+
+	  @PostMapping("/upload")
+	  public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) {
+	    String message = "";
+	    try {
+	      storageService.save(file);
+
+	      message = "Uploaded the file successfully: " + file.getOriginalFilename();
+	      return ResponseEntity.status(HttpStatus.OK).body(message);
+	    } catch (Exception e) {
+	      message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+	      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+	    }
+	  }
+
+	  @GetMapping("/files")
+	  public ResponseEntity<List<FileInfo>> getListFiles() {
+	    List<FileInfo> fileInfos = storageService.loadAll().map(path -> {
+	      String filename = path.getFileName().toString();
+	      String url = MvcUriComponentsBuilder
+	          .fromMethodName(UploadFileHandler.class, "getFile", path.getFileName().toString()).build().toString();
+
+	      return new FileInfo(filename, url);
+	    }).collect(Collectors.toList());
+
+	    return ResponseEntity.status(HttpStatus.OK).body(fileInfos);
+	  }
+
+	  @GetMapping("/files/{filename:.+}")
+	  @ResponseBody
+	  public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+	    Resource file = storageService.load(filename);
+	    return ResponseEntity.ok()
+	        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+	  }
+}
